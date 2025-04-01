269c9acc1d2feb6ea7ceff7dc4292f7acb255bf3
Trinadh Machanavazzala <trinadhm469@gmail.com>
2025-03-31 22:40:06 -0400

    Mongoose setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Book Store Server is Running!!')
+    res.send('Book Store Server is Running!!')
 })
 
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+main().then(() => console.log("MongoDB success!")).catch(err => console.log(err));
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
